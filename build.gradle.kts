e149643a743f35a39fa722498d59b68a58d6cb9e
Minecraftschurli <minecraftschurli@gmail.com>
2024-02-17 14:01:15 +0100

    use source set configuration from helper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,29 @@
 plugins {
+    idea
     id ("com.github.minecraftschurlimods.helperplugin")
 }
 
-sourceSets.register("api")
-
-val neo by configurations.creating
-configurations.implementation.configure { extendsFrom(neo) }
-configurations.named("apiCompileOnly").configure { extendsFrom(neo) }
+helper.withApiSourceSet()
 
 dependencies {
-    neo(helper.neoforge())
-    implementation(sourceSets.api.output)
+    implementation(helper.neoforge())
     compileOnly("org.jetbrains:annotations:23.0.0")
     "apiCompileOnly"("org.jetbrains:annotations:23.0.0")
 }
 
-tasks.jar {
-    from(sourceSets.main.output)
-    from(sourceSets.api.output)
-}
-
-tasks.named<Jar>("sourcesJar") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    from(sourceSets.main.allSource)
-    from(sourceSets.api.allSource)
-}
-
-val apiJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-    dependsOn(tasks.named("apiClasses"))
-    archiveClassifier.set("api")
-    from(sourceSets.api.allSource)
-    from(sourceSets.api.output)
-}
-
-artifacts.archives(apiJar)
-
-helper.publication.apply {
-    artifact(apiJar)
-    pom {
-        organization {
-            name = "Minecraftschurli Mods"
-            url = "https://github.com/MinecraftschurliMods"
-        }
-        developers {
-            developer {
-                id = "minecraftschurli"
-                name = "Minecraftschurli"
-                email = "minecraftschurli@gmail.com"
-                organization = "Minecraftschurli Mods"
-                organizationUrl = "https://github.com/Minecraftschurli"
-                timezone = "Europe/Vienna"
-            }
+helper.publication.pom {
+    organization {
+        name = "Minecraftschurli Mods"
+        url = "https://github.com/MinecraftschurliMods"
+    }
+    developers {
+        developer {
+            id = "minecraftschurli"
+            name = "Minecraftschurli"
+            email = "minecraftschurli@gmail.com"
+            organization = "Minecraftschurli Mods"
+            organizationUrl = "https://github.com/Minecraftschurli"
+            timezone = "Europe/Vienna"
         }
     }
 }
-
-val SourceSetContainer.api: Provider<SourceSet> get() = named("api")
-val Provider<SourceSet>.allSource: Provider<SourceDirectorySet> get() = map { it.allSource }
-val Provider<SourceSet>.output: Provider<SourceSetOutput> get() = map { it.output }
